84006e47765ad813c44f229abbac259f7cf014a1
Narsimha1408 <tirumala.narsimha@gmail.com>
2025-04-15 13:18:26 +0530

    done password hashing for user register route using bcryptjs package

--- server/routes/UserRoute.js
+++ server/routes/UserRoute.js
@@ -1,18 +1,28 @@
 const express=require("express")
 const router=express.Router()
 const UserModel=require("../models/UserModel.js")
+const bcrypt=require("bcryptjs")
 
 router.post("/register",async (req,res)=>{
     //checking if already a same user exists in db
     
     try{
-        const userExist=UserModel.findOne({mail: req.body.mail})
+        const userExist=await UserModel.findOne({mail: req.body.mail})
+       
         if(userExist){
+            
             res.send({
                 success: true,
                 message: "user already exists",
             })
         }
+
+        //hasing the password with added salt using bycrypt npm package
+        const salt=await bcrypt.genSalt(10)
+        const hashedPassword= await bcrypt.hash(req.body.password, salt)
+        req.body.password=hashedPassword
+
+
         const newUser = await UserModel(req.body)
         await newUser.save()
         res.send({
