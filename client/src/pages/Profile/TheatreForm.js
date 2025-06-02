029c646fe3efb56f33e1ed44e5712ada380bc659
Narsimha1408 <tirumala.narsimha@gmail.com>
2025-06-03 01:45:48 +0530

    Designed a table structure using antD for theatre list

--- client/src/pages/Profile/TheatreForm.js
+++ client/src/pages/Profile/TheatreForm.js
@@ -24,7 +24,7 @@ const TheatreFormComponent = ({isModalOpen, setIsModalOpen, getTheatresData}) =>
       }
     }
     catch(error){
-      console.log(error)
+      message.error(error.message)
     }
     
   }
--- client/src/pages/Profile/TheatreList.js
+++ client/src/pages/Profile/TheatreList.js
@@ -1,38 +1,106 @@
 import React, { useEffect, useState } from "react";
-import {Button} from 'antd';
-import { EditFilled } from '@ant-design/icons'
+import {Button, message} from 'antd';
+import { EditFilled, EditOutlined, DeleteOutlined } from '@ant-design/icons'
 import TheatreFormComponent from "./TheatreForm";
 import { useSelector } from "react-redux";
 import { GetAllTheatres } from "../../apicalls/theatres";
+import { Table, } from 'antd';
 
 const TheatreList=()=>{
     const {user} = useSelector((state)=>state.user)
     const [isModalOpen, setIsModalOpen] = useState(false)
+    const [theatres, setTheatres] = useState(null);
 
 
     const getTheatresData= async ()=>{
         try{
-        const theatresData = await GetAllTheatres({owner: user._id})
-            console.log(theatresData)
+            const response = await GetAllTheatres({owner: user._id})
+            console.log(response)
+
+            if(response.success){
+                const allTheatres = response.data
+
+                //adding key which is used for antd table
+                setTheatres(allTheatres.map((item)=>{    
+                    return {...item, key: `theatre${item._id}`}
+                }))
+            }else{
+                message.error(response.message)
+            }
+            
         }
         catch(error){
-            console.log(error)
+            message.error(error.message);
+        }
+
+    }
+
+    //these cols are antd format by default
+    const columns=[
+        {
+          title: 'Name',
+          dataIndex: 'name',
+          key: 'name'
+        },
+        {
+          title: 'Address',
+          dataIndex: 'address',
+          key: 'address',
+        },
+        {
+          title: 'Phone Number',
+          dataIndex: 'phone',
+          key: 'phone',
+        },
+        {
+          title: 'Email',
+          dataIndex: 'email',
+          key: 'email'
+        },
+        {
+          title: 'Status',
+          dataIndex: 'status',
+          render: (status, data)=>{
+            if(data.status){ 
+                return "Approved"
+            }else{
+                return "Pending/ Blocked"
+            }
+          }
+        },
+        {
+            title : 'Action',
+            dataIndex: 'action',
+            render: (text, data) =>{
+                return (
+                    <div className="d-flex gap-10">
+                        <Button><EditOutlined/></Button>
+                        <Button><DeleteOutlined/></Button>
+                    </div>
+                )
+            }
         }
 
 
+    ]
 
-    }
+    //getting the theatres list when component mounted
     useEffect(() => {
         if (user) {
         getTheatresData();
         }
     }, [user]);
+
     return (
         <>
             <div>
                 <div style={{ display: 'flex', justifyContent: 'end'}}>
                     <Button type="primary" onClick={()=>{setIsModalOpen(true)}}>Add Theatres</Button>
                 </div>  
+                {/* adding antd table */}
+
+                <Table columns={columns} dataSource={theatres} />
+
                 <div>
                     <button onClick={()=>{setIsModalOpen(true)}}><EditFilled /></button>
                 </div>
